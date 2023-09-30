28402e1705f6a25fc0baf3c8361f3c5f463dbd18
Platon <83786662+Alphorov@users.noreply.github.com>
2023-09-30 21:03:46 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_application/src/config/theme/app_themes.dart';
-import 'package:news_application/src/feature/daily_news/presentation/bloc/article/remote/remote_aricle_bloc.dart';
-import 'package:news_application/src/feature/daily_news/presentation/pages/daily_news_home.dart';
-import 'package:news_application/src/injection_container.dart';
+import 'package:news_app_clean_architecture/config/routes/routes.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/pages/home/daily_news.dart';
+import 'config/theme/app_themes.dart';
+import 'features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'injection_container.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<RemoteAricleBloc>(
-      create: (context) => sl()..add(const RemoteAricleFetch()),
+    return BlocProvider<RemoteArticlesBloc>(
+      create: (context) => sl()..add(const GetArticles()),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: theme(),
-        home: const DailyNewsPage(),
-        builder: (context, child) => child!,
+        onGenerateRoute: AppRoutes.onGenerateRoutes,
+        home: const DailyNews()
       ),
     );
   }
 }
+
--- lib/src/core/constants/constants.dart
+++
@@ -1,6 +0,0 @@
-const newsApiBaseUrl = 'https://newsapi.org/v2';
-const newsApiKey = '2a5c9248554049a1b31344d7676b8cbb';
-const countryQuery = 'us';
-const categoryQuery = 'general';
-const String kDefaultImage =
-    "https://www.google.com/search?q=default+image&client=firefox-b-d&sxsrf=APq-WBskmtr-ix6NUAqqiHFNpsJX6JSOTg:1650026644151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjEi_qfjJb3AhXvQd8KHd02BKUQ_AUoAXoECAEQAw#imgrc=A0pMe2lq2NT_jM";
